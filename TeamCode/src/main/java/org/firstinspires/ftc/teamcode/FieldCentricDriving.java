e5649f03c86473677b290664d737b63b0ef9545b
ko-ey <ko.eyisi@gmail.com>
2025-09-18 16:13:57 -0400

    Update FieldCentricDriving.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FieldCentricDriving.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FieldCentricDriving.java
@@ -1,9 +1,13 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.IMU;
+
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
 
 @TeleOp
 public class FieldCentricDriving extends OpMode {
@@ -12,6 +16,17 @@ public class FieldCentricDriving extends OpMode {
     DcMotor right_front;
     DcMotor left_back;
     DcMotor right_back;
+
+    IMU imu;
+    double Yaw;
+    double Pitch;
+    double Roll;
+    YawPitchRollAngles robotOrientation;
+
+    double left_front_velocity = 0;
+    double right_front_velocity = 0;
+    double left_back_velocity = 0;
+    double right_back_velocity = 0;
 
     @Override
     public void init() {
@@ -27,10 +42,60 @@ public class FieldCentricDriving extends OpMode {
 
         right_front.setDirection(DcMotorSimple.Direction.REVERSE);
         right_back.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        IMU.Parameters myIMUparameter;
+
+        myIMUparameter = new IMU.Parameters(
+                new RevHubOrientationOnRobot(
+                        RevHubOrientationOnRobot.LogoFacingDirection.RIGHT,
+                        RevHubOrientationOnRobot.UsbFacingDirection.UP
+                )
+        );
+
+        imu.initialize(myIMUparameter);
+
+        robotOrientation = imu.getRobotYawPitchRollAngles();
+
+        Yaw = robotOrientation.getYaw();
+        Pitch = robotOrientation.getPitch();
+        Roll = robotOrientation.getRoll();
+
     }
 
     @Override
     public void loop() {
-        
+        Yaw = robotOrientation.getYaw();
+        Pitch = robotOrientation.getPitch();
+        Roll = robotOrientation.getRoll();
+
+        //movement
+
+        right_front_velocity = gamepad1.left_stick_y - gamepad1.left_stick_x;
+        left_front_velocity = gamepad1.left_stick_y + gamepad1.left_stick_x;
+
+        left_back_velocity = gamepad1.left_stick_y - gamepad1.left_stick_x;
+        right_back_velocity = gamepad1.left_stick_y + gamepad1.left_stick_x;
+
+        //rotation
+
+        right_front_velocity += -gamepad1.right_stick_x;
+        right_back_velocity += -gamepad1.right_stick_x;
+
+        left_front_velocity += gamepad1.right_stick_x;
+        left_back_velocity += gamepad1.right_stick_x;
+
+
+        right_front.setPower(right_front_velocity);
+        right_back.setPower(right_back_velocity);
+        left_front.setPower(left_front_velocity);
+        left_back.setPower(left_back_velocity);
+
+//        if(gamepad1.left_stick_y != 0) {
+//
+//        }
+//
+//        if(gamepad1.right_stick_y != 0) {
+//
+//        }
     }
 }
