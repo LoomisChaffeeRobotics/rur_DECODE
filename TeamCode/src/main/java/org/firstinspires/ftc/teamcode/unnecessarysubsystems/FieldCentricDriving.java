88d9c0fc21fb77fddfe756f52ffcdc5597d13983
violin2024 <musichu20171819@gmail.com>
2025-10-15 07:42:21 -0400

    i misspelled systems
    😭

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/unnecessarysubsystems/FieldCentricDriving.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/unnecessarysubsystems/FieldCentricDriving.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.unnecessarysubsystem;
+package org.firstinspires.ftc.teamcode.unnecessarysubsystems;
 
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
@@ -7,7 +7,6 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.IMU;
 
-import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
 
 @TeleOp
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/unnecessarysubsystems/unnecessaryLimeLightTurretSystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/unnecessarysubsystems/unnecessaryLimeLightTurretSystem.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.unnecessarysubsystem;
+package org.firstinspires.ftc.teamcode.unnecessarysubsystems;
 
 
 
