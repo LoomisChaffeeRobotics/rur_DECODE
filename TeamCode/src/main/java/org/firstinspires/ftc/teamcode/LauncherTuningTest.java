4c868758fd29cd5086c9a861e6ba5bac3803922d
Dylanqw <145394726+Dylanqw@users.noreply.github.com>
2025-10-30 17:23:39 -0400

    0.0

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LauncherTuningTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LauncherTuningTest.java
@@ -10,7 +10,6 @@ public class LauncherTuningTest extends OpMode {
 
     DcMotor bottom_motor;
     DcMotor top_motor;
-
     double bottom_power = 0;
     double top_power = 0;
 
@@ -18,22 +17,36 @@ public class LauncherTuningTest extends OpMode {
 
     @Override
     public void init() {
-        bottom_motor = hardwareMap.get(DcMotor.class, "launcher1");
-        top_motor = hardwareMap.get(DcMotor.class, "launcher2;");
+        bottom_motor = hardwareMap.get(DcMotor.class, "launcher1"); // port 2
+        top_motor = hardwareMap.get(DcMotor.class, "launcher2"); // port 0
         elapsedTime = new ElapsedTime();
     }
 
     @Override
     public void loop() {
-        if (gamepad1.a && elapsedTime.time() > 0.2) {
+        if (gamepad1.dpad_down && elapsedTime.time() > 0.2) {
             bottom_power += 0.1;
             bottom_motor.setPower(bottom_power);
             elapsedTime.reset();
         }
-        if (gamepad1.b && elapsedTime.time() > 0.2) {
-            top_power += 0.1;
+        if (gamepad1.dpad_up && elapsedTime.time() > 0.2) {
+            bottom_power -= 0.1;
             bottom_motor.setPower(top_power);
             elapsedTime.reset();
         }
+        if (gamepad1.y && elapsedTime.time() > 0.2) {
+            top_power += 0.1;
+            top_motor.setPower(bottom_power);
+            elapsedTime.reset();
+        }
+        if (gamepad1.a && elapsedTime.time() > 0.2) {
+            top_power -= 0.1;
+            top_motor.setPower(top_power);
+            elapsedTime.reset();
+        }
+
+        telemetry.addData("top: ",top_power);
+        telemetry.addData("bottom: ",bottom_power);
+        telemetry.update();
     }
 }
