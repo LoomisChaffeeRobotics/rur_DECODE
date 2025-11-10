80f82064f57e692f692c9c838844bedaf240c9a9
superhappydancer <tideriah@internal.loomis.org>
2025-11-10 17:09:37 -0500

    Update Servoturnertester.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Servoturnertester.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Servoturnertester.java
@@ -1,8 +1,9 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Servo;
-
+@TeleOp
 public class Servoturnertester extends OpMode {
     Servo supercoolservo;
     double number = 0d;
