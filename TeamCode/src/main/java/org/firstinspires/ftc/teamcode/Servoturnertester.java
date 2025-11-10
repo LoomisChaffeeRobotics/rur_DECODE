f759082db2404a0f9ccff5bf7e2e42e776631cfe
superhappydancer <tideriah@internal.loomis.org>
2025-11-10 16:30:20 -0500

    Create Servoturnertester.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Servoturnertester.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class Servoturnertester extends OpMode {
+    Servo supercoolservo;
+    double number = 0d;
+
+    @Override
+    public void init() {
+        supercoolservo = hardwareMap.get(Servo.class, "servo");
+    }
+
+    @Override
+    public void loop() {
+        if (gamepad1.dpad_down){number = number + .01;}
+        else if (gamepad1.dpad_up){number = number -.01;}
+
+supercoolservo.setPosition(number);
+
+
+telemetry.addData("position: ",
+        supercoolservo.getPosition());
+telemetry.update();
+
+    }
+}
