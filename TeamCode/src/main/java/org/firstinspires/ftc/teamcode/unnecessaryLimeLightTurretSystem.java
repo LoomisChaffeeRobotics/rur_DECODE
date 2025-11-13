d28c3bbed88ab0e743ab5f6a4b51873ab6568d41
violin2024 <musichu20171819@gmail.com>
2025-11-13 16:01:47 -0500

    i just splpit the method into two
    why

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/unnecessaryLimeLightTurretSystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/unnecessaryLimeLightTurretSystem.java
@@ -49,6 +49,19 @@ public class unnecessaryLimeLightTurretSystem{
 
 
     }
+    public void turntoAT() {
+        result = limelight.getLatestResult();
+        botpose = (result != null) ? result.getBotpose() : botpose;
+        botposeangle = botpose.getOrientation().getYaw(AngleUnit.RADIANS);
+        angleerror = targetangle - botposeangle;
+        if (angleerror < -1 || angleerror > 180) {
+            sv.setPower(-0.1);
+        } else if (angleerror > 1 && angleerror < 165) {
+            sv.setPower(0.1);
+        } else if (angleerror < 1 && angleerror > -1){
+            sv.setPower(0);
+        }
+    }
 
 
     public double getDistance_from_apriltag(double value) {
@@ -73,59 +86,8 @@ public class unnecessaryLimeLightTurretSystem{
 //        } else if (angleerror < 1 && angleerror > -1){
 //            sv.setPower(0);
 //        }
-
-        // thtey wouldh ave to hold down the button - might not be necessary if a better solution is found or something
-
         status = limelight.getStatus();
-//        telemetry.addData("Name", "%s",
-//                status.getName());
-//        telemetry.addData("LL", "Temp: %.1fC, CPU: %.1f%%, FPS: %d",
-//                status.getTemp(), status.getCpu(),(int)status.getFps());
-//        telemetry.addData("Pipeline", "Index: %d, Type: %s",
-//                status.getPipelineIndex(), status.getPipelineType());
-//            double captureLatency = result.getCaptureLatency();
-//            double targetingLatency = result.getTargetingLatency();
-//            double parseLatency = result.getParseLatency();
-//            telemetry.addData("LL Latency", captureLatency + targetingLatency);
-//            telemetry.addData("Parse Latency", parseLatency);
-//            telemetry.addData("PythonOutput", java.util.Arrays.toString(result.getPythonOutput()));
-//
-//            telemetry.addData("tx", result.getTx());
-//            telemetry.addData("txnc", result.getTxNC());
-//            telemetry.addData("ty", result.getTy());
-//            telemetry.addData("tync", result.getTyNC());
-
-
-            // Access barcode results
-//            List<LLResultTypes.BarcodeResult> barcodeResults = result.getBarcodeResults();
-//            for (LLResultTypes.BarcodeResult br : barcodeResults) {
-//                telemetry.addData("Barcode", "Data: %s", br.getData());
-//            }
-//
-//            // Access classifier results
-//            List<LLResultTypes.ClassifierResult> classifierResults = result.getClassifierResults();
-//            for (LLResultTypes.ClassifierResult cr : classifierResults) {
-//                telemetry.addData("Classifier", "Class: %s, Confidence: %.2f", cr.getClassName(), cr.getConfidence());
-//            }
-//
-//            // Access detector results
-//            List<LLResultTypes.DetectorResult> detectorResults = result.getDetectorResults();
-//            for (LLResultTypes.DetectorResult dr : detectorResults) {
-//                telemetry.addData("Detector", "Class: %s, Area: %.2f", dr.getClassName(), dr.getTargetArea());
-//            }
-
-            // Access fiducial results
-
-//            telemetry.update();
-//
-
-//            // Access color results
-//            List<LLResultTypes.ColorResult> colorResults = result.getColorResults();
-//            for (LLResultTypes.ColorResult cr : colorResults) {
-//                telemetry.addData("Color", "X: %.2f, Y: %.2f", cr.getTargetXDegrees(), cr.getTargetYDegrees());
-//            }
-
-            return distance_from_apriltag;
+        return distance_from_apriltag;
     }
 }
 
